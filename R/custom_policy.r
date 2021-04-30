8f2db6b8b19603c6ae6d2fe10b208c3adaba4021
ValerCheck <mypowerismind@gmail.com>
2021-05-01 01:02:08 +0300

    Add policy and custom policy wrappers

---
+++ R/custom_policy.r
@@ -0,0 +1,64 @@
+#' Custom policy
+#'
+#' @param name policy name
+#' @param resources vector of resource names
+#' @param env environment as a part of policy container, which can be used as a parameter in custom function
+#' @param select_function resource selection function
+#' @param initial_container container of values, from which policy object will be constructed
+#'
+#' @return new custom policy object
+#' @export
+new_custom_policy <- function(name = character(), resources = character(), env = NA,
+                              select_function = get_simmer_policy_resources, initial_container = list()) {
+  policy <- new_policy(name, resources, env = env, class = "custom_policy", initial_container = initial_container)
+  policy$select_function <- adapt_policy_selection_function(select_function, policy)
+  policy
+}
+
+
+# S3 Methods --------------------------------------------------------------
+
+#' Get policy name for custom policy
+#'
+#' In other words, there are 2 iterations of resources selection:
+#' 1. Produce list of resource names. Production logic can incapsulate decision logic.
+#' 2. Simmer default algorithm selection.
+#'
+#' @param policy policy object
+#'
+#' @return policy name with respect to "simmer"
+#' @export
+get_policy_name.custom_policy <- function(policy) {
+  simmer_policy_name_match()
+}
+
+#' Get resource function for custom policy S3 method
+#'
+#' In any "simmer" scenario there is no explicit way to define custom policy with custom any.
+#' However, custom policy can be provided by function which produces a list of resources for selection.
+#' If such function will provide only one value then any "simmer" policy function will only choose the one.
+#' Technically, it means that custom function will provide "optimal" value for it's algorithm.
+#'
+#' @param policy policy object
+#'
+#' @return resource function
+#' @export
+get_resources_function.custom_policy <- function(policy) {
+  policy$select_function(policy)
+}
+
+
+#' Wrapper for custom policy selection function
+#' @keywords internal
+#' @export
+adapt_policy_selection_function <- function(select_function, policy) {
+  arguments <- methods::formalArgs(select_function)
+
+  if ("policy" %in% arguments)
+    select_function
+  else
+    function(policy) {
+      force(policy)
+      function() purrr::lift_dl(select_function)(policy[arguments])
+    }
+}
---
+++ R/policy.r
@@ -0,0 +1,120 @@
+#' Simmer policy wrapper
+#'
+#' @param name policy name (either default or custom)
+#' @param resources vector of resource names to enumerate over
+#' @param ... additional arguments
+#' @param class custom class name, used for extensions
+#' @param initial_container container of items from which policy object is going to be created
+#'
+#' @return new policy object wrapper
+#' @export
+new_policy <- function(name = character(), resources = character(), ..., class = character(), initial_container = list())
+{
+  dots = list(...)
+
+  initial_container[names(dots)] <- dots
+  initial_container[c("name", "resources")] <- list(name, resources)
+
+  structure(initial_container, class = c(class, "policy"))
+}
+
+#' Check object is policy
+#'
+#' @param x object to test
+#'
+#' @return TRUE or FALSE
+#' @export
+is_policy <- function(x) {
+  is(x, "policy")
+}
+
+
+# S3 Generics -------------------------------------------------------------
+
+#' S3 genric for policy initialization
+#'
+#' @param policy policy wrapper object
+#' @param ... additional parameters to enrich with
+#'
+#' @export
+policy_initialize <- function(policy, ...) {
+  UseMethod("policy_initialize")
+}
+
+#' S3 generic for getting resources from policy
+#'
+#' @param policy policy wrapper object
+#'
+#' @export
+get_resources_function <- function(policy) {
+  UseMethod("get_resources_function")
+}
+
+#' S3 generic for getting policy name
+#'
+#' @param policy policy wrapper object
+#'
+#' @export
+get_policy_name <- function(policy) {
+  UseMethod("get_policy_name")
+}
+
+
+# S3 Methods --------------------------------------------------------------
+
+#' Policy initialize function
+#'
+#' @param policy policy wrapper
+#' @param ... arguments to enrich with
+#'
+#' @return policy wrapper
+#' @export
+policy_initialize.policy <- function(policy, ...) {
+  dots <- list(...)
+
+  if (!rlang::is_named(dots))
+    stop("Only named arguments must be provided!")
+
+  policy[names(dots)] <- dots
+  policy
+}
+
+#' Get policy resources function
+#'
+#' @param policy policy wrapper object
+#'
+#' @return policy function to be used
+#' @export
+get_resources_function.policy <- function(policy) {
+  get_simmer_policy_resources(policy)
+}
+
+#' Get policy name
+#'
+#' @param policy policy wrapper object
+#'
+#' @return policy name consumed by simmer
+#' @export
+get_policy_name.policy <- function(policy) {
+  simmer_policy_name_match(policy$name)
+}
+
+
+# Default policy object methods for simmer policies -----------------------
+
+#' Simmer default policy
+#' @keywords internal
+#' @export
+get_simmer_policy_resources <- function(policy) {
+  simmer_policy_name_match(policy$name)
+  function() policy$resources
+}
+
+#' Simmer default policy name match
+#' @keywords internal
+#' @export
+simmer_policy_name_match <- function(policyName = c("shortest-queue", "shortest-queue-available",
+                                                "round-robin", "round-robin-available", "first-available",
+                                                "random", "random-available")) {
+  match.arg(policyName)
+}
