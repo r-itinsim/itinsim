381c0bd399cb074b8f1d2ef59b8d18b9c518c990
ValerCheck <mypowerismind@gmail.com>
2022-05-04 17:01:11 +0300

    Add get_distribution_function to the project

--- R/utils.r
+++ R/utils.r
@@ -16,3 +16,47 @@ prepare_args_from_caller <- function(func, args) {
 
   args[dplyr::setdiff(names(args), not_mapped_args_names)]
 }
+
+
+#' S3 method for as.data.frame when 'density' type is provided
+#'
+#' @param .density Density object from stats package
+#'
+#' @return data frame which contains only 'x' and 'y' values from 'density' object
+#' @export
+as.data.frame.density <- function(.density)
+{
+  data.frame(y = .density$y, x = .density$x)
+}
+
+
+#' Convert vector of values from some column in data frame to distribution function
+#' Original approach was taken from answer here:
+#' https://stackoverflow.com/questions/32871602/r-generate-data-from-a-probability-density-distribution
+#'
+#' @param data Provided data.frame
+#' @param column Column name
+#' @param only.value If is equal to TRUE then generator function will always return single value, otherwise it will return pairs of x and y
+#'
+#' @return function which accepts number and returns random value from generated distribution
+#' @export
+get_distribution_function <- function(data, column, only.value = T)
+{
+  approx_data <- data %>%
+    dplyr::pull({{column}}) %>%
+    stats::density(n = nrow(data), bw = 1) %>%
+    as.data.frame() %>%
+    dplyr::mutate(relcumsum = cumsum(y)/sum(y)) %>%
+    dplyr::select(x = relcumsum, y = x) %>%
+    as.list()
+
+  function(n)
+  {
+    result <- approx_data %>%
+      stats::approx(xout = runif(n), ties = min) %>%
+      as.data.frame() %>%
+      dplyr::mutate(y = ifelse(y < 0 | is.na(y), 0, floor(y)))
+
+    if (only.value) result$y else result
+  }
+}
